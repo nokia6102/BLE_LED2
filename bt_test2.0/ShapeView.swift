06106ee8b619b9aa3ff6a0fb6903084fcf2989dd
Me <nokia6101@gmail.com>
2018-03-09 15:53:09 +0800

    First Commit! Swift版本手機 控制LED字幕 app

---
+++ bt_test2.0/ShapeView.swift
@@ -0,0 +1,91 @@
+//
+//  ShapeView.swift
+//  IOS10DrawShapesTutorial
+//
+//  Created by Arthur Knopper on 01/10/2016.
+//  Copyright © 2016 Arthur Knopper. All rights reserved.
+//
+
+import UIKit
+
+extension CALayer {
+    
+    func colorOfPoint(point:CGPoint) -> CGColor {
+        
+        var pixel: [CUnsignedChar] = [0, 0, 0, 0]
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        
+        let context = CGContext(data: &pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        context!.translateBy(x: -point.x, y: -point.y)
+        
+        self.render(in: context!)
+        
+        let red: CGFloat   = CGFloat(pixel[0]) / 255.0
+        let green: CGFloat = CGFloat(pixel[1]) / 255.0
+        let blue: CGFloat  = CGFloat(pixel[2]) / 255.0
+        let alpha: CGFloat = CGFloat(pixel[3]) / 255.0
+        
+        let color = UIColor(red:red, green: green, blue:blue, alpha:alpha)
+        
+        return color.cgColor
+    }
+}
+
+class ShapeView: UIView {
+    @objc var currentShapeType: Int = 0
+    @objc var currentText: String = ""
+    @objc init(frame: CGRect, text : String)
+    {
+        super.init(frame: frame)
+        self.currentText = text
+     
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+      
+        drawText()
+    
+    }
+    //---
+    
+    
+    
+    
+    @objc func drawText()
+    {
+        let str = self.currentText
+     //   let len = self.currentText.lengthOfBytes(using: .utf8)
+        
+        let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 16, height: 18))      //swift 4
+        let font = UIFont.systemFont(ofSize: 16
+        )            //Fix to swift 4
+        
+        let color = UIColor.black
+        let style = NSMutableParagraphStyle()
+        
+        style.alignment = NSTextAlignment.left
+        (str as NSString).draw(in: rect, withAttributes: [NSAttributedStringKey.font:font,NSAttributedStringKey.foregroundColor:color,NSAttributedStringKey.paragraphStyle:style])
+    }
+    
+
+    
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+    
+    
+}
---
+++ bt_test2.0/ViewController.swift
@@ -0,0 +1,367 @@
+//
+//  ViewController.swift
+//  bt_test2.0
+//
+//  Created  on 2017/9/11.
+//  Copyright © 2017年 Albert. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+extension String {
+    subscript(value: PartialRangeUpTo<Int>) -> Substring {
+        get {
+            return self[..<index(startIndex, offsetBy: value.upperBound)]
+        }
+    }
+    
+    subscript(value: PartialRangeThrough<Int>) -> Substring {
+        get {
+            return self[...index(startIndex, offsetBy: value.upperBound)]
+        }
+    }
+    
+    subscript(value: PartialRangeFrom<Int>) -> Substring {
+        get {
+            return self[index(startIndex, offsetBy: value.lowerBound)...]
+        }
+    }
+}
+extension Data {
+    func hex(separator:String = "") -> String {
+        return (self.map { String(format: "%02X", $0) }).joined(separator: separator)
+    }
+}
+
+extension Decimal {
+    var uint16Value:UInt16
+    {
+        return NSDecimalNumber(decimal: self).uint16Value
+    }
+}
+
+
+
+
+class ViewController: UIViewController, BluetoothSerialDelegate
+
+{
+//MARK: IBOutlets
+    @IBOutlet weak var txtInfo: UITextView!
+    @IBOutlet weak var myText: UITextView!
+
+    
+var martixColor = [[Int]](repeating: [Int](repeating: 0, count: 16), count: 16)
+
+    var line = [UInt8]()
+    var totalLine = [UInt8]()
+    
+//MARK: IBActions
+    @IBAction func btnAutomaticallyFlared(_ sender: UIButton)
+    {
+        
+        let uiText = myText.text
+        
+       // let uiTextLen = myText.text.lengthOfBytes(using: .utf8) / 3
+        let uiTextLen = Array(uiText!).count
+    print ("***")
+        print(uiTextLen)
+     
+    //   totalLine.removeAll()
+        for x in 0 ..< uiTextLen
+        {
+            totalLine.removeAll()
+            
+            let singleText = String(Array(uiText!)[x])
+            totalLine.append(contentsOf: getText(inpText: singleText))
+            print (">>>\(singleText),\(totalLine)")
+      
+        print ("---")
+        print (totalLine)
+          //   }
+      //  var sendString0 = "SetMsg="
+         
+   var sendString0 = (x == 0 ? "SetMsg=" : "AddMsg=")
+      var newData0 = sendString0.data(using: String.Encoding.utf8)
+
+     //   var c0 = newData0
+        var size : UInt8 = 2
+        var dataArray :UInt8 = UInt8(totalLine.count)
+        var total:Int16 = Int16(size + dataArray)
+        var highByte:UInt8 = UInt8(total >> 8)
+        let lowByte:UInt8 = UInt8(total & 0xff)
+        
+        var emptyNSdata = NSMutableData()
+        
+        //key command
+        emptyNSdata.append(newData0!)
+        
+        var endMarker = NSData(bytes: [UInt8(highByte), lowByte] , length: 2)
+        
+        emptyNSdata.append(endMarker as Data)
+        
+//        var dataBytes = NSData(bytes: line, length:  32)
+          var dataBytes = NSData(bytes: totalLine, length:  totalLine.count)
+
+        emptyNSdata.append(dataBytes as Data)
+        
+   
+        
+        print (">:")
+        print (emptyNSdata)
+        print ("-:")
+        
+        serial.addDataToDevice( emptyNSdata as Data )
+    }
+    
+  }
+
+    @IBAction func btnUpFlared(_ sender: UIButton)    //close
+    {
+        serial.sendMessageToDevice("c")
+    }
+    
+    @IBAction func btnDownFlared(_ sender: UIButton)
+    {
+        serial.sendMessageToDevice("o")
+    }
+    
+    @IBAction func btnLeftFlared(_ sender: UIButton)    //opne
+    {
+        serial.sendMessageToDevice("o")
+    }
+    
+    @IBAction func btnRightFlared(_ sender: UIButton)
+    {
+        serial.sendMessageToDevice("C")
+    }
+    
+    @IBAction func btnStopFlared(_ sender: UIButton)
+    {
+        serial.sendMessageToDevice("z")  //藍芽傳送字串“z"
+    }
+    
+    
+    func getText (inpText:String) -> [UInt8]
+    {
+    
+        let myView  = ShapeView(frame:CGRect(x:25,y:200,width:320,height:16),text: inpText)
+        myView.backgroundColor = UIColor.white
+        view.addSubview(myView)
+        
+        var myColor : Float
+        
+        
+        for i in 0..<16
+        {
+            for j in 0..<16
+            {
+                print (myView.layer.colorOfPoint(point: CGPoint(x: i, y: j) ))
+                myColor = Float(myView.layer.colorOfPoint(point: CGPoint(x: i, y: j)).components![0])
+                
+                print ("color:\(myColor)")
+                
+                if myColor >= 0.5
+                {
+                    martixColor[i][j] = 0
+                    
+                }
+                else
+                {
+                    martixColor[i][j] = 1
+                }
+                
+            }
+        }
+        
+        
+        //        var line = [Int]()
+        line.removeAll()
+        for l in 0..<16
+        {
+            
+            var sum : Decimal
+            sum = 0
+            for k in 0..<16
+            {
+                
+                if martixColor[k][l] == 0
+                {
+                    print("_", terminator:"")
+                }
+                else
+                {
+                    sum +=  (pow(2, 15-k))
+                    print("1", terminator:"")
+                }
+                // print (sum)
+                
+            }
+            
+            
+            line.append( UInt8((sum.uint16Value >> 8)&0xff) )
+            
+            line.append(UInt8(sum.uint16Value & 0xff))
+            
+            
+            print (",")
+        }
+        
+        for o in 0..<32
+        {
+            print ("%02x",line[o])
+        }
+        
+        return line
+    }
+    
+    func showText ()
+    {
+        let sText =  myText.text
+        let sLen =  10
+        let myView  = ShapeView(frame:CGRect(x:25,y:200,width:320,height:16),text: sText!)
+        myView.backgroundColor = UIColor.white
+        view.addSubview(myView)
+        
+        var myColor : Float
+        
+        // var martixColor = [[Int]]()
+        
+        
+        
+        
+        for i in 0..<320
+        {
+            for j in 0..<16
+            {
+                print (myView.layer.colorOfPoint(point: CGPoint(x: i, y: j) ))
+                myColor = Float(myView.layer.colorOfPoint(point: CGPoint(x: i, y: j)).components![0])
+                
+                print ("color:\(myColor)")
+                
+                if myColor >= 0.5
+                {
+                    martixColor[i][j] = 0
+                    
+                }
+                else
+                {
+                    martixColor[i][j] = 1
+                }
+                
+            }
+        }
+        
+        
+//        var line = [Int]()
+        line.removeAll()
+        for l in 0..<16
+        {
+            
+            var sum : Decimal
+            sum = 0
+            for k in 0..<16
+            {
+                
+                if martixColor[k][l] == 0
+                {
+                    print("_", terminator:"")
+                }
+                else
+                {
+                    sum +=  (pow(2, 15-k))
+                    print("1", terminator:"")
+                }
+                // print (sum)
+                
+            }
+           
+            
+               line.append( UInt8((sum.uint16Value >> 8)&0xff) )
+            
+               line.append(UInt8(sum.uint16Value & 0xff))
+       
+            
+            print (",")
+        }
+        
+        for o in 0..<32
+        {
+            print ("%02x",line[o])
+        }
+        
+    }
+//MARK: Variables
+    
+    /// The peripherals that have been discovered (no duplicates and sorted by asc RSSI)
+    var peripherals: [(peripheral: CBPeripheral, RSSI: Float)] = []
+    
+    /// The peripheral the user has selected
+    var selectedPeripheral: CBPeripheral?
+    
+    /// Progress hud shown
+    var progressHUD: MBProgressHUD?
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+   
+        
+        serial = BluetoothSerial(delegate: self)
+        
+        // tell the delegate to notificate US instead of the previous view if something happens
+        serial.delegate = self
+        
+        if serial.centralManager.state == .poweredOn
+        {
+            txtInfo.text! = "Bluetooth turned on"
+//            return
+        }
+
+    }
+    deinit
+    {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//MARK: BluetoothSerialDelegate
+    
+    func serialDidReceiveString(_ message: String)
+    {
+
+
+    }
+    
+    func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?)
+    {
+
+    }
+    
+    func serialDidChangeState()
+    {
+        var msg = ""
+        
+        switch serial.centralManager.state
+        {
+        case .poweredOff:
+            msg = "bluetooth is off"
+        case .poweredOn:
+            msg = "bluetooth is on"
+        case .unsupported:
+            msg = "bluetooth is unsupported"
+        default:
+            break
+        }
+        print("START:\(msg)")
+    }
+
+}
+
